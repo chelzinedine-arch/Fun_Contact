180b569fa377c4e31e5dd39bfdff437817a776f2
Zinedine <chelzinedine@gmail.com>
2026-07-29 21:57:00 +0200

    Fix Railway deployment

--- ff.py
+++ ff.py
@@ -293,5 +293,7 @@ def logout():
     return redirect("/signin")
 
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
